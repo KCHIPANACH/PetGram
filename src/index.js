22aa6cc97f093eae65689e61e4ca62fe19924e09
Kevin Chipana <kvn.chipana@gmail.com>
2020-06-06 18:55:10 -0500

    subiendo a master

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+  <h1> Hola Kevin Como estas, preparado para react con midudev !!! </h1>,
+  document.getElementById('app')
+)
