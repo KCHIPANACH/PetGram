3fa658c6e81c577c69a219ed159a2c9db122fae0
Kevin Chipana <kvn.chipana@gmail.com>
2020-06-07 01:01:36 -0500

    Añadiendo hooks y fetching a photos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
 /* Base de datos temporal hasta hacer el fetching con la api Graph */
-import { categories } from '../Prueba/db.json'
+/* import { categories } from '../Prueba/db.json' */
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    window.fetch('https://petgrambackend.now.sh/categories')
+      .then(resp => resp.json())
+      .then(response => setCategories(response))
+  }, [])
+
   return (
     <List>
       {
--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { PhotoCard } from '../PhotoCard'
 
 export const ListOfPhotoCards = () => {
+  const [photos, setPhotos] = useState([])
+
+  useEffect(() => {
+    window.fetch('https://petgrambackend.now.sh/photos')
+      .then(resp => resp.json())
+      .then(response => setPhotos(response))
+  }, [])
+
   return (
     <ul>
       {
-        [1, 2, 3, 4].map((item, key) => <PhotoCard key={key} />)
+        photos.map((item, key) => <PhotoCard key={key} {...item} />)
       }
     </ul>
   )
