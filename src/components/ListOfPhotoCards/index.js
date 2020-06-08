b1026b0925a1e08d259af3f78ff0d263829c06da
Kevin Chipana <kvn.chipana@gmail.com>
2020-06-08 17:11:40 -0500

    añadiendo api y dependencias graphql,  apollo cliente

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { useFetchData } from '../../components/hooks/useFetchData'
+/* import { useFetchData } from '../../components/hooks/useFetchData' */
 
-export const ListOfPhotoCards = () => {
-  const [photos] = useFetchData('https://petgrambackend.now.sh/photos')
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+const withPhotos = graphql(gql`
+query getPhotos{
+  photos{
+    id,
+    categoryId,
+    src,
+    likes,
+    liked,
+    userId
+  }
+}
+`)
+
+/* Componentes de orden superior: Son aquellas funciones que se le pasa como parametro un componente y devuelve otro componente ya sea con mejoras o props inyectadas  */
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+/*   const [photos] = useFetchData('https://petgrambackend.now.sh/photos') */
 
   return (
     <ul>
@@ -13,3 +31,5 @@ export const ListOfPhotoCards = () => {
     </ul>
   )
 }
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
 import { App } from './App'
 
+const client = new ApolloClient({
+  uri: 'https://petgrambackend.now.sh/graphql'
+})
+
 ReactDom.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 )
