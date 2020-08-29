1fcc62ee9de67dbd02b705637d68589aa46f90e7
Kevin Chipana <kvn.chipana@gmail.com>
2020-08-29 00:51:26 -0500

    añadiendo public a icons

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
       icons: [
         {
           src: path.resolve('src/assets/icon.png'),
-          sizes: [96, 128, 192, 256, 384, 512]
+          sizes: [96, 128, 192, 256, 384, 512],
+          publicPath:'/'
         }
       ]
     }),
